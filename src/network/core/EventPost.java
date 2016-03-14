78fbb9ea0f0a6d5e5f0765aee1888d6a764292c2
kerzal <olemartin94@hotmail.com>
2016-03-14 10:20:41 +0100

    backup

---
+++ src/network/core/EventPost.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package network.core;
+
+/**
+ *
+ * @author OleMartin
+ */
+public class EventPost extends Post
+{
+    private String eventType;
+
+    public EventPost(String author, String eventType)
+    {
+        super(author);
+        this.eventType = eventType; 
+    }
+    
+    
+    
+    
+    
+    
+    
+}
